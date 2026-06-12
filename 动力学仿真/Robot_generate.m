1bd6b272581c0165a671b4693b99406c67feb163
jt <92149912@qq.com>
2026-06-12 18:50:54 +0800

    update 20260612 18.50

---
+++ 动力学仿真/Robot_generate.m
@@ -0,0 +1,13 @@
+clc;
+clear;
+tar.POS_e = [1.80 0 1.80]';
+tar.ORI_e = cy(pi/2);
+RP_data = Module_Lib();
+[n, module_raw, install_raw, align_raw, sequence_raw] = generate_valid_config_x(RP_data);
+[module_out, install_out, align_out, sequence_out, num_exp, is_valid, ~] = expand_module_units(module_raw(1:n),install_raw(1:n),align_raw(1:n),sequence_raw(1:n),RP_data);
+LP = LP_generate(module_out, install_out, align_out, sequence_out, RP_data);
+SV = SV_generate(LP);
+
+
+
+
